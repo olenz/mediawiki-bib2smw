da921ff550f4b8c1c044d001bb89c3dd39580068
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2013-06-14 11:09:19 +0200

    Made pyGenXML more generic.

--- pyGenXML.sh
+++ pyGenXML.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+if test -z "$1"; then
+	echo "Usage: $0 BIBNAME"
+	exit 2
+fi
+BIBNAME=$1
 WORKDIR=`pwd`/work
 
-BIBFILE="$WORKDIR/icp.bib"
-XMLFILE="$WORKDIR/icp.xml"
+BIBFILE="$WORKDIR/$BIBNAME.bib"
+XMLFILE="$WORKDIR/$BIBNAME.xml"
 echo "Prefixing stuff ..."
 #the parser sucks at {"} and screws up ...
 sed s/{\"}/\\\&quote\;/ $BIBFILE |sed s/{\"}/\\\&quote\;/ |sed s/{\"}/\\\&quote\;/ |sed s/{\"}/\\\&quote\;/ |sed s/{\"}/\\\&quote\;/ |sed s/{\"}/\\\&quote\;/ |sed s/'{[^\]*{[^\]*".*}.*}.#'// | sed s/'#.{[^\]*{[^\]*".*}.*}'// |sed s/'\\&\/'/\&amp\;/ >$BIBFILE.tmp
@@ -20,4 +25,4 @@ s/\&\#3\;/?/ |sed -e s/\&\#2\;/?/ |sed -e s/\&\#2\;/?/ |sed -e s/\&\#2\;/?/ \
 |sed -e s/\&\#4\;/?/ |sed -e s/\&\</?\</ | sed -e s/bibtex:// | \
 sed -e s/bibtex:// | sed  s/\0x1B//g | sed  s/\0x0B//g | sed  s/\0x0C//g | sed  s/\0x02//g  > $XMLFILE
 
-php ../SemanticMediaWiki/maintenance/SMW_refreshData.php --page="Database_icp"
+php ../SemanticMediaWiki/maintenance/SMW_refreshData.php --page="Database_$BIBNAME"
